416165fe7205ee6b46fb2b3f3e1c4d9954ffc52f
David Bauer <mail@david-bauer.net>
2019-03-05 13:51:46 +0100

    version 0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='pymeshviewer',
-    version='0.1.2',
+    version='0.1.3',
 
     description='Parser for Meshviewer nodelists and topology files',
 
